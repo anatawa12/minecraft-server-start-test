2533203713fa01e498c03b06d2ed13a799112a37
anatawa12 <anatawa12@icloud.com>
2021-06-02 21:54:09 +0900

    add tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,28 +1,109 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import {parseParameters} from '../src/parameters'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
+interface AllParameters {
+  server_type: string
+  version: string
+  work_dir: string
+  sleep_time: string
+  timeout: string
+  world_data: string
+  mods_dir: string
+  mod_jar: string
+  config_dir: string
+  config_jar: string
+}
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
+async function wrapped(params: Partial<AllParameters>) {
+  const env_names: {[P in keyof AllParameters]: string} = {
+    server_type: 'INPUT_SERVER_TYPE',
+    version: 'INPUT_VERSION',
+    work_dir: 'INPUT_WORK_DIR',
+    sleep_time: 'INPUT_SLEEP_TIME',
+    timeout: 'INPUT_TIMEOUT',
+    world_data: 'INPUT_WORLD_DATA',
+    mods_dir: 'INPUT_MODS_DIR',
+    mod_jar: 'INPUT_MOD_JAR',
+    config_dir: 'INPUT_CONFIG_DIR',
+    config_jar: 'INPUT_CONFIG_JAR',
   }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+
+  const defaults: Partial<AllParameters> = {
+    server_type: 'forge',
+    sleep_time: '0s',
+  }
+
+  function set_env(
+    parameters: Partial<Omit<AllParameters, 'version'>> &
+      Pick<AllParameters, 'version'>,
+  ) {
+    parameters = Object.assign({}, defaults, parameters)
+    for (let [key, value] of Object.entries(env_names)) {
+      if (parameters[key as keyof AllParameters]) {
+        process.env[value] = parameters[key as keyof AllParameters]
+      } else {
+        delete process.env[value]
+      }
+    }
+  }
+
+  try {
+    set_env(Object.assign({}, params, {version: '1.12.2-14.23.5.2855'}))
+    return await parseParameters()
+  } finally {
+    for (let value of Object.values(env_names)) {
+      delete process.env[value]
+    }
+  }
+}
+
+test('parse parameters', async () => {
+  await expect(wrapped({})).resolves.toMatchObject({
+    sleepTimeConfig: '0 seconds',
+    timeout: null,
+    worldData: '',
+    modJar: '',
+    modsDir: '',
+    configFile: '',
+    configDir: '',
+  })
+})
+
+describe('sleep_time', () => {
+  test('s', async () => {
+    await expect(wrapped({sleep_time: '3s'})).resolves.toHaveProperty(
+      'sleepTimeConfig',
+      '3 seconds',
+    )
+  })
+  test('second', async () => {
+    await expect(wrapped({sleep_time: '3second'})).resolves.toHaveProperty(
+      'sleepTimeConfig',
+      '3 seconds',
+    )
+  })
+  test('seconds', async () => {
+    await expect(wrapped({sleep_time: '3seconds'})).resolves.toHaveProperty(
+      'sleepTimeConfig',
+      '3 seconds',
+    )
+  })
+  test('t', async () => {
+    await expect(wrapped({sleep_time: '3t'})).resolves.toHaveProperty(
+      'sleepTimeConfig',
+      '3 ticks',
+    )
+  })
+  test('tick', async () => {
+    await expect(wrapped({sleep_time: '3tick'})).resolves.toHaveProperty(
+      'sleepTimeConfig',
+      '3 ticks',
+    )
+  })
+  test('ticks', async () => {
+    await expect(wrapped({sleep_time: '3ticks'})).resolves.toHaveProperty(
+      'sleepTimeConfig',
+      '3 ticks',
+    )
+  })
 })
